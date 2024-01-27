596899487bbb3b3d5830275098ddab45f9c11f81
XirTech <john.darlucio022@gmail.com>
2024-01-27 10:24:14 +0800

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,147 @@
+// NavBar
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
+// Particles
+particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 80,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: "#ffffff",
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000",
+        },
+        polygon: {
+          nb_sides: 5,
+        },
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false,
+        },
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false,
+        },
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 6,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse",
+        },
+        onclick: {
+          enable: true,
+          mode: "push",
+        },
+        resize: true,
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1,
+          },
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+        push: {
+          particles_nb: 4,
+        },
+        remove: {
+          particles_nb: 2,
+        },
+      },
+    },
+    retina_detect: true,
+  });
+  function changeText(button) {
+      button.textContent = "Download";
+  }
+  function resetText(button) {
+      button.textContent = "Resume";
+  }
+  // Loop Typing Text
+  const texts = ["Web Developer", "IT Specialist", "Mobile Developer", "IT Technician"];
+  
+  const typingSpeed = 100; // Adjust this value to control the typing speed (in milliseconds)
+  
+  const typingElement = document.getElementById("typing-text");
+  
+  // Define function typeText
+  function typeText(text, index) {
+    if (index < text.length) {
+      typingElement.textContent += text.charAt(index);
+      index++;
+      setTimeout(() => typeText(text, index), typingSpeed);
+    } else {
+      setTimeout(() => {
+        typingElement.textContent = ""; // Clear the text content after the whole text is typed
+  
+        // Move to the next text in the array
+        const nextIndex = (texts.indexOf(text) + 1) % texts.length;
+        typeText(texts[nextIndex], 0);
+      }, 1000); // Add a delay before starting the next loop
+    }
+  }
+  
+  typeText(texts[0], 0); // Start the typing animation with the first text
+  
---
+++ readme.md
@@ -0,0 +1,43 @@
+# My Second Made Personal Portfolio
+
+Welcome to the Awesome Project repository! This project is all about [briefly describe your project].
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+To get started with the Awesome Project, follow these steps:
+
+1. Clone the repository to your local machine.
+    ```bash
+    git clone https://github.com/TrixDiaz/TrixDiaz.github.io.git
+    ```
+
+2. [Additional installation steps if any...]
+
+## Usage
+Open the Code index.html right click and choose show preview
+
+## Contributing
+We welcome contributions! If you'd like to contribute to the Awesome Project, please follow these guidelines:
+- Fork the repository
+- Create a new branch (`git checkout -b feature/your-feature`)
+- Make your changes and commit them (`git commit -am 'Add some feature'`)
+- Push to the branch (`git push origin feature/your-feature`)
+- Create a pull request
+
+## License
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+### Check out my Personal Portfolio
+🌐 [Trix Diaz Portfolio](https://www.trixdiaz.github.io)
+Short description: Explore my personal portfolio to discover more about my skills, projects, and experience.
+
+---
+
+Feel free to customize this template according to your project's details and preferences. Happy coding!
